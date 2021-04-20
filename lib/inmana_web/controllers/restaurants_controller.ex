44f12075ecf2620af916eba80a0177976f2191f8
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-20 11:35:59 -0300

    fix: restaurantsController

--- lib/inmana_web/controllers/restaurants_controller.ex
+++ lib/inmana_web/controllers/restaurants_controller.ex
@@ -1,11 +1,11 @@
-defmodule InmanaWeb.WelcomeController do
+defmodule InmanaWeb.RestaurantsController do
   use InmanaWeb, :controller
 
   alias Inmana.Restaurant
   alias Inmana.Restaurants.Create
 
   def create(conn, params) do
-    with {:ok, %Restaurante{} = restaurant} <- Create.call(params) do
+    with {:ok, %Restaurant{} = restaurant} <- Create.call(params) do
       conn
       |> put_status(:ok)
       |> render("create.json", restaurant: restaurant)
